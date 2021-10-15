d6fe4aa5394099b408c71c64694aae606623089f
Assylzhan Zharzhanov <asik.zharzhanov@gmail.com>
2021-10-15 15:48:32 +0600

    add letsencrypt sh

--- nginx/init_letsencrypt.sh
+++ nginx/init_letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(api-ads.region.app www.api-ads.region.app)
+domains=(api-ads.region.app)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="hello@treelab.kz" # Adding a valid address is strongly recommended
