043ba563c2028e3d04cd50f19dad06be13c39371
Assylzhan Zharzhanov <asik.zharzhanov@gmail.com>
2021-10-25 09:03:00 +0600

    fix:

--- models/advert.go
+++ models/advert.go
@@ -10,6 +10,7 @@ type Advert struct {
 	ID               primitive.ObjectID `json:"id" bson:"_id,omitempty"`
 	UserID           primitive.ObjectID `json:"user_id" bson:"user_id"`
 	Status           string             `json:"status" bson:"status"`
+	Reason           string       		`json:"reason,omitempty" bson:"reason,omitempty"`
 	City             string             `json:"city" bson:"city"`
 	Category         string             `json:"category" bson:"category" form:"category"`
 	Phone            string             `json:"phone" bson:"phone"`
@@ -18,7 +19,7 @@ type Advert struct {
 	Description      string             `json:"description" bson:"description" form:"description"`
 	RentType         string             `json:"rent_type" bson:"rent_type" form:"rent_type"`
 	Price            int                `json:"price" bson:"price" form:"price"`
-	Images           []Image `json:"images" bson:"images" form:"images"`
+	Images           []Image 		    `json:"images" bson:"images" form:"images"`
 	HasAdvertisement bool               `json:"has_advertisement" bson:"has_advertisement,omitempty"`
 	Advertisement    Advertisement      `json:"advertisement" bson:"advertisement,omitempty"`
 	Feedbacks        []Feedback         `json:"feedbacks" bson:"feedbacks,omitempty"`
@@ -29,6 +30,7 @@ type Advert struct {
 type AdvertOutput struct {
 	ID               primitive.ObjectID `json:"id" bson:"_id,omitempty"`
 	Status           string             `json:"status" bson:"status"`
+	Reason           string       		`json:"reason" bson:"reason"`
 	City             string             `json:"city" bson:"city"`
 	Category         string             `json:"category" bson:"category"`
 	Phone            string             `json:"phone" bson:"phone"`
